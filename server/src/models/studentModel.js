fe3828aab6f0c716ddf2039fa4e4c67019327a4e
AhmadRamadhan-bit <ahmadoneperson@gmail.com>
2021-12-23 08:15:42 +0700

    creating student schema

---
+++ server/src/models/studentModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    rollno: {
+      type: Number,
+    },
+  },
+  {
+    collection: "student",
+  }
+);
+
+module.exports = mongoose.model("Student", studentSchema);
